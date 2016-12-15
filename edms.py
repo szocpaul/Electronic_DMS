a6ec7f288ab92b9f2dcc342ab17cadf8addc1c6f
Pal Majoros <pal.majoros@hu.ibm.com>
2016-12-15 22:00:00 +0100

    add db configurations to main file

--- edms.py
+++ edms.py
@@ -1,10 +1,12 @@
 import repository
 import config
+import database
 
 
 def main():
     configuration = config.parse('config.toml')
     repository.get_instance(configuration)
+    database.get_instance(configuration)
 
 
 if __name__ == '__main__':
