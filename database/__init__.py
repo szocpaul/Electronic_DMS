9d7f1b509b25b1da525d4436600bf2dc4e3a16cb
Pal Majoros <pal.majoros@hu.ibm.com>
2016-12-15 20:38:11 +0100

    Add  template for database class

---
+++ database/__init__.py
@@ -0,0 +1,45 @@
+def get_instance(configuration):
+    if "database" not in configuration:
+        raise KeyError("Did not found database in configuration")
+    if "type" not in configuration['database']:
+        raise KeyError("Could not determine database type to use")
+
+    dbtype = configuration['database']['type']
+    if dbtype == 'sqlite':
+        from . import sqlite
+        return sqlite.EdmsSqlite(configuration['database'])
+    else:
+        raise ValueError("Invalid database type")
+
+
+def virtual(_):
+    def raiser(_):
+        raise NotImplementedError
+
+    return raiser
+
+
+class EdmsDatabase(object):
+    @virtual
+    def save(self, document):
+        pass
+
+    @virtual
+    def load(self, uuid=None, raw_uuid=None):
+        pass
+
+    @virtual
+    def search(self, tags, from_date, to_date):
+        pass
+
+    @virtual
+    def tag_count(self):
+        pass
+
+    @virtual
+    def remove(self):
+        pass
+
+
+class ExistError(Exception):
+    pass
