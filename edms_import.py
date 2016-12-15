a173b1cee70a5d42687d619e364b567e72361a5d
Pal Majoros <pal.majoros@hu.ibm.com>
2016-12-15 21:34:18 +0100

    Add repository and db configurations

--- edms_import.py
+++ edms_import.py
@@ -4,17 +4,32 @@ import uuid as uuid_lib
 import repository
 import config
 import dateutil.parser
+import database
 
-with open() as _file:
-    docs = json.load(_file)
-    for data in docs:
-        doc = document.Document(
-            uuid=uuid_lib.UUID(data['uuid']),
-            title=data['name'],
-            creation_date=dateutil.parser.parse(data['creation_date']).date(),
-            document_date=dateutil.parser.parse(data['document_date']).date(),
-            author=data['author'],
-            description=data['description'],
-            state=data['state'],
-            is_public=data['is_public'])
 
+def main(file_name):
+    db, _ = get_db()
+
+    with open(file_name) as _file:
+        docs = json.load(_file)
+        for data in docs:
+            doc = document.Document(
+                uuid=uuid_lib.UUID(data['uuid']),
+                title=data['name'],
+                creation_date=dateutil.parser.parse(data['creation_date']).date(),
+                document_date=dateutil.parser.parse(data['document_date']).date(),
+                author=data['author'],
+                description=data['description'],
+                state=data['state'],
+                is_public=data['is_public'])
+            try:
+                db.save(doc)
+            except database.ExistError:
+                print(str(doc.uuid) + " already exist")
+
+
+def get_db():
+    configuration = config.parse('config.toml')
+    db = database.get_instance(configuration)
+    repo = repository.get_instance(configuration)
+    return db, repo
